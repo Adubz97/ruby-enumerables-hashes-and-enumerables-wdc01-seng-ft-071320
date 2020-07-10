bd62331d12504bc1ae33a73712268bb767670d68
Abdullah Albasha <abdullah.a.albasha@gmail.com>
2020-07-10 13:53:59 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -13,5 +13,9 @@
 
 def select_winner(passengers)
   # add the code snippet here!
-  passengers
+  winner = ""
+passengers.each do |suite, name|
+  if suite == :suite_a && name.start_with?("A")
+    winner = name
+  end
 end
